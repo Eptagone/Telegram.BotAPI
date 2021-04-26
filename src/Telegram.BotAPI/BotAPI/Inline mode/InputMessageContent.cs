3596dceb773f30a870e2d96f3171e984551ee9e4
Quetzal Rivera <QuetzalDeveloper@outlook.com>
2021-04-26 17:44:38 -0500

    Added the class InputInvoiceMessageContent
    Signed-off-by: Quetzal Rivera <QuetzalDeveloper@outlook.com>

--- src/Telegram.BotAPI/BotAPI/Inline mode/InputMessageContent.cs
+++ src/Telegram.BotAPI/BotAPI/Inline mode/InputMessageContent.cs
@@ -3,11 +3,12 @@
 
 namespace Telegram.BotAPI.InlineMode
 {
-    /// <summary>This object represents the content of a message to be sent as a result of an inline query. Telegram clients currently support the following 4 types: <br /><br />
+    /// <summary>This object represents the content of a message to be sent as a result of an inline query. Telegram clients currently support the following 5 types: <br /><br />
     /// • <see cref="InputTextMessageContent"/><br />
     /// • <see cref="InputLocationMessageContent"/><br />
     /// • <see cref="InputVenueMessageContent"/><br />
-    /// • <see cref="InputContactMessageContent"/></summary>
+    /// • <see cref="InputContactMessageContent"/><br />
+    /// • <see cref="InputInvoiceMessageContent"/></summary>
     public abstract class InputMessageContent
     {
         //Base empty class
