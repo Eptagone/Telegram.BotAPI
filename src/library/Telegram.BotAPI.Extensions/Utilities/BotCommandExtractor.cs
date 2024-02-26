a416d99e8c8cd358d8a01c1acad0e710c658e8dc
Quetzal Rivera <eptadot799@outlook.com>
2024-02-25 20:41:32 -0600

    Add support to detect commands from captions

--- src/library/Telegram.BotAPI.Extensions/Utilities/BotCommandExtractor.cs
+++ src/library/Telegram.BotAPI.Extensions/Utilities/BotCommandExtractor.cs
@@ -12,46 +12,48 @@ namespace Telegram.BotAPI.Extensions;
 /// </summary>
 sealed class BotCommandExtractor : IBotCommandExtractor
 {
-	private const string COMMAND_EXTRACT_PATTERN = @"^\/(?<cmd>\w*)(?:|@{0})(?:$|\s(?<params>.*))";
-	private readonly Regex cmdRegex;
+    private const string COMMAND_EXTRACT_PATTERN = @"^\/(?<cmd>\w*)(?:|@{0})(?:$|\s(?<params>.*))";
+    private readonly Regex cmdRegex;
 
-	/// <summary>
-	/// Initializes a new instance of the <see cref="BotCommandExtractor"/> class.
-	/// </summary>
-	/// <param name="username">The bot's username.</param>
-	/// <param name="useRegexCompiled">Set true to use regex compiled option.</param>
-	public BotCommandExtractor(string username, bool useRegexCompiled)
-	{
-		var pattern = string.Format(
-			CultureInfo.InvariantCulture,
-			COMMAND_EXTRACT_PATTERN,
-			username
-		);
-		var options = RegexOptions.IgnoreCase;
-		if (useRegexCompiled)
-		{
-			options |= RegexOptions.Compiled;
-		}
-		this.cmdRegex = new Regex(pattern, options);
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BotCommandExtractor"/> class.
+    /// </summary>
+    /// <param name="username">The bot's username.</param>
+    /// <param name="useRegexCompiled">Set true to use regex compiled option.</param>
+    public BotCommandExtractor(string username, bool useRegexCompiled)
+    {
+        var pattern = string.Format(
+            CultureInfo.InvariantCulture,
+            COMMAND_EXTRACT_PATTERN,
+            username
+        );
+        var options = RegexOptions.IgnoreCase;
+        if (useRegexCompiled)
+        {
+            options |= RegexOptions.Compiled;
+        }
+        this.cmdRegex = new Regex(pattern, options);
+    }
 
-	/// <inheritdoc/>
-	public bool HasCommand(Message message)
-	{
-		return !string.IsNullOrEmpty(message.Text) && this.cmdRegex.IsMatch(message.Text);
-	}
+    /// <inheritdoc/>
+    public bool HasCommand(Message message)
+    {
+        var text = message.Text ?? message.Caption;
+        return !string.IsNullOrEmpty(text) && this.cmdRegex.IsMatch(text);
+    }
 
-	/// <inheritdoc/>
-	public (string commandName, string args) ExtractCommand(Message message)
-	{
-		var match = this.cmdRegex.Match(message.Text);
-		if (!match.Success)
-		{
-			throw new InvalidOperationException("The message does not contain a command.");
-		}
+    /// <inheritdoc/>
+    public (string commandName, string args) ExtractCommand(Message message)
+    {
+        var text = message.Text ?? message.Caption;
+        var match = this.cmdRegex.Match(text);
+        if (!match.Success)
+        {
+            throw new InvalidOperationException("The message does not contain a command.");
+        }
 
-		var name = match.Groups["cmd"].Value;
-		var args = match.Groups["params"].Value;
-		return (name, args);
-	}
+        var name = match.Groups["cmd"].Value;
+        var args = match.Groups["params"].Value;
+        return (name, args);
+    }
 }
