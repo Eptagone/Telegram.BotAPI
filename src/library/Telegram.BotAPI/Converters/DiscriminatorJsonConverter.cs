446fc83dc4d11fdbe0ef3b5c5aa031c742e83790
Quetzal Rivera <eptadot799@outlook.com>
2024-05-13 23:05:52 -0600

    feat: Improve DiscriminatorJsonConverter feat: Update package version

--- src/library/Telegram.BotAPI/Converters/DiscriminatorJsonConverter.cs
+++ src/library/Telegram.BotAPI/Converters/DiscriminatorJsonConverter.cs
@@ -14,85 +14,123 @@ namespace Telegram.BotAPI.Converters;
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public abstract class DiscriminatorJsonConverter<T> : JsonConverter<T>
-	where T : class
+    where T : class
 {
-	/// <summary>
-	/// Gets the name of the property that contains the discriminator value.
-	/// </summary>
-	public abstract string PropertyName { get; }
+    /// <summary>
+    /// Gets the name of the property that contains the discriminator value.
+    /// </summary>
+    public abstract string PropertyName { get; }
 
-	private static ReadOnlyDictionary<string, Type>? discriminatorMapping;
+    private static ReadOnlyDictionary<string, IEnumerable<Type>>? discriminatorMapping;
 
-	/// <inheritdoc />
-	public override void Write(
-		Utf8JsonWriter writer,
-		T value,
-		JsonSerializerOptions options
-	)
-	{
-		if (value == null)
-		{
-			throw new ArgumentNullException(nameof(value));
-		}
-		var type = value.GetType();
-		JsonSerializer.Serialize(writer, value, type, options);
-	}
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        if (value == null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+        var type = value.GetType();
+        JsonSerializer.Serialize(writer, value, type, options);
+    }
 
-	/// <inheritdoc />
-	public override T? Read(
-		ref Utf8JsonReader reader,
-		Type typeToConvert,
-		JsonSerializerOptions options
-	)
-	{
-		var jsonDoc = JsonSerializer.Deserialize<JsonDocument>(ref reader, options)!;
-		var prop = jsonDoc.RootElement.GetProperty(this.PropertyName);
-		var rawText = jsonDoc.RootElement.GetRawText();
-		var discriminatorValue =
-			prop.GetString()
-			?? throw new JsonException($"Property type of {this.PropertyName} must be a String.");
+    /// <inheritdoc />
+    public override T? Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        var jsonDoc = JsonSerializer.Deserialize<JsonDocument>(ref reader, options)!;
+        var prop = jsonDoc.RootElement.GetProperty(this.PropertyName);
+        var rawText = jsonDoc.RootElement.GetRawText();
+        var discriminatorValue =
+            prop.GetString()
+            ?? throw new JsonException($"Property type of {this.PropertyName} must be a String.");
 
-		// Load mapping if it hasn't been loaded yet
-		discriminatorMapping ??= LoadDiscriminatorMapping(this.PropertyName);
+        // Load mapping if it hasn't been loaded yet
+        discriminatorMapping ??= LoadDiscriminatorMapping(this.PropertyName);
 
-		if (discriminatorMapping.TryGetValue(discriminatorValue, out var type))
-		{
-			var @object =
-				JsonSerializer.Deserialize(rawText, type, options)
-				?? throw new JsonException($"Json object is not a valid {type.Name}.");
-			return (T)@object;
-		}
+        if (discriminatorMapping.TryGetValue(discriminatorValue, out var types))
+        {
+            Type type;
+            if (types.Count() > 1)
+            {
+                // Select the type with more propertie present in the json
+                type = types
+                    .OrderByDescending(t =>
+                    {
+                        var properties = t.GetProperties()
+                            .Where(p => p.GetCustomAttribute<JsonPropertyNameAttribute>() != null);
+                        var count = 0;
+                        foreach (var property in properties)
+                        {
+                            var propertyName = property
+                                .GetCustomAttribute<JsonPropertyNameAttribute>()
+                                ?.Name;
+                            if (
+                                !string.IsNullOrEmpty(propertyName)
+                                && jsonDoc.RootElement.TryGetProperty(property.Name, out _)
+                            )
+                            {
+                                count++;
+                            }
+                        }
+                        return count;
+                    })
+                    .First();
+            }
+            else
+            {
+                type = types.First();
+            }
 
-		throw new InvalidOperationException(
-			$"Cannot find type for discriminator value {discriminatorValue}"
-		);
-	}
+            var @object =
+                JsonSerializer.Deserialize(rawText, type, options)
+                ?? throw new JsonException($"Json object is not a valid {type.Name}.");
+            return (T)@object;
+        }
 
-	private static ReadOnlyDictionary<string, Type> LoadDiscriminatorMapping(string propertyName)
-	{
-		// Get all classes that inherit from T
-		var classes = typeof(T).Assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(T)));
-		// Create a dictionary of the discriminator values and the corresponding type
-		var mapping = new Dictionary<string, Type>();
-		foreach (var c in classes)
-		{
-			// Creates a new instance of the class without calling the constructor
-			var instance = FormatterServices.GetUninitializedObject(c);
-			// Try to get the discriminator value
-			var discriminator = c.GetProperties()
-				.FirstOrDefault(p =>
-					p.GetCustomAttribute<JsonPropertyNameAttribute>()?.Name == propertyName
-				)
-				?.GetValue(instance);
-			if (discriminator is string discriminatorValue)
-			{
-				mapping.Add(discriminatorValue, c);
-				continue;
-			}
+        throw new InvalidOperationException(
+            $"Cannot find type for discriminator value {discriminatorValue}"
+        );
+    }
 
-			throw new InvalidOperationException($"Cannot get discriminator value from {c.Name}");
-		}
+    private static ReadOnlyDictionary<string, IEnumerable<Type>> LoadDiscriminatorMapping(
+        string propertyName
+    )
+    {
+        // Get all classes that inherit from T
+        var classes = typeof(T).Assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(T)));
+        // Create a dictionary of the discriminator values and the corresponding type
+        var mappings = new Dictionary<string, IEnumerable<Type>>();
+        foreach (var c in classes)
+        {
+            // Creates a new instance of the class without calling the constructor
+            var instance = FormatterServices.GetUninitializedObject(c);
+            // Try to get the discriminator value
+            var discriminator = c.GetProperties()
+                .FirstOrDefault(p =>
+                    p.GetCustomAttribute<JsonPropertyNameAttribute>()?.Name == propertyName
+                )
+                ?.GetValue(instance);
+            if (discriminator is string discriminatorValue)
+            {
+                if (mappings.TryGetValue(discriminatorValue, out IEnumerable<Type>? value))
+                {
+                    var types = value.Concat([c]);
+                    mappings[discriminatorValue] = types;
+                }
+                else
+                {
+                    mappings.Add(discriminatorValue, [c]);
+                }
+                continue;
+            }
 
-		return new ReadOnlyDictionary<string, Type>(mapping);
-	}
+            throw new InvalidOperationException($"Cannot get discriminator value from {c.Name}");
+        }
+
+        return new ReadOnlyDictionary<string, IEnumerable<Type>>(mappings);
+    }
 }
--- src/tests/Telegram.BotAPI.Tests/Serialization.cs
+++ src/tests/Telegram.BotAPI.Tests/Serialization.cs
@@ -1,34 +1,95 @@
 ﻿// Copyright (c) 2024 Quetzal Rivera.
 // Licensed under the MIT License, See LICENCE in the project root for license information.
 
+using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Telegram.BotAPI.InlineMode;
 using Telegram.BotAPI.UpdatingMessages;
 
 namespace Telegram.BotAPI.Tests;
 
 public sealed class Serialization(ITestOutputHelper outputHelper)
 {
-	private static readonly JsonSerializerOptions options =
-		new() { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+    private static readonly JsonSerializerOptions options =
+        new() { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
 
-	private readonly ITestOutputHelper _outputHelper = outputHelper;
+    private readonly ITestOutputHelper _outputHelper = outputHelper;
 
-	[Fact]
-	public void SerializeAnyObject()
-	{
-		var obj = new EditMessageLiveLocationArgs(1, 0);
+    [Fact]
+    public void SerializeEditMessageLiveLocationArgs()
+    {
+        var obj = new EditMessageLiveLocationArgs(1, 0);
 
-		var rawText = JsonSerializer.Serialize(obj, options);
-		this._outputHelper.WriteLine(rawText);
-	}
+        var rawText = JsonSerializer.Serialize(obj, options);
+        this._outputHelper.WriteLine(rawText);
+    }
 
-	[Fact]
-	public void SerializeSendGameArgs()
-	{
-		var args = new SendGameArgs(777777, "Welcome to my game");
+    [Fact]
+    public void SerializeReplyMarkup()
+    {
+        ReplyMarkup obj = new InlineKeyboardMarkup(
+            [
+                [new InlineKeyboardButton("Button") { CallbackData = "data" }]
+            ]
+        );
 
-		var rawText = JsonSerializer.Serialize(args, options);
-		this._outputHelper.WriteLine(rawText);
-	}
+        var rawText = JsonSerializer.Serialize(obj, options);
+        this._outputHelper.WriteLine(rawText);
+        var deserializeMarkup = JsonSerializer.Deserialize<ReplyMarkup>(rawText, options);
+        Assert.IsType<InlineKeyboardMarkup>(deserializeMarkup);
+        var inlineKeyboard = ((InlineKeyboardMarkup)deserializeMarkup).InlineKeyboard;
+        Assert.Equal("Button", inlineKeyboard.First().First().Text);
+        Assert.Equal("data", inlineKeyboard.First().First().CallbackData);
+    }
+
+    [Fact]
+    public void SerializeSendGameArgs()
+    {
+        var args = new SendGameArgs(777777, "Welcome to my game");
+
+        var rawText = JsonSerializer.Serialize(args, options);
+        this._outputHelper.WriteLine(rawText);
+        var deserializeArgs = JsonSerializer.Deserialize<SendGameArgs>(rawText, options);
+        Assert.Equal(777777, deserializeArgs.ChatId);
+        Assert.Equal("Welcome to my game", deserializeArgs.GameShortName);
+    }
+
+    [Fact]
+    public void SerializeQueryResultWithTextMessageContent()
+    {
+        var result = new InlineQueryResultArticle
+        {
+            Id = "1",
+            Title = "Title",
+            InputMessageContent = new InputTextMessageContent("Hello, World!")
+            {
+                ParseMode = "MarkdownV2"
+            },
+            ReplyMarkup = new InlineKeyboardMarkup(
+                [
+                    [new InlineKeyboardButton("Button") { CallbackData = "data" }]
+                ]
+            )
+        };
+
+        var rawText = JsonSerializer.Serialize(result, options);
+        this._outputHelper.WriteLine(rawText);
+        var deserializeResult = JsonSerializer.Deserialize<InlineQueryResult>(rawText, options);
+        Assert.IsType<InlineQueryResultArticle>(deserializeResult);
+        Assert.Equal(
+            "data",
+            deserializeResult.ReplyMarkup.InlineKeyboard.First().First().CallbackData
+        );
+        Assert.IsType<InputTextMessageContent>(
+            ((InlineQueryResultArticle)deserializeResult).InputMessageContent
+        );
+        Assert.Equal(
+            "Hello, World!",
+            (
+                (InputTextMessageContent)
+                    ((InlineQueryResultArticle)deserializeResult).InputMessageContent
+            ).MessageText
+        );
+    }
 }
